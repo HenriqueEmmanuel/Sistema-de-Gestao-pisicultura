fe6ec9a9947829752b8dd7558f4ed645acba37b2
HenriqueEmmanuel <henriqueemmanuel807@gmail.com>
2025-06-26 17:38:32 -0300

    Formulação da tela dos tanques e formulario de cadastro de tanques

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import bancodedados  # quando criuar o banco coloca aqui
 
 
 
@@ -9,6 +10,10 @@ def index(request):
 
 def tank(request):
     return render(request, 'front/tank.html')
+    tanques = Tanque.objects.all()
+    context = {'tem_tanques': tanques.exists(), 'tanques': tanques}
+    return render(request, 'front/tank.html', context)
+
 
 def pagina_erro_404(request):
     return render(request, 'front/pagina_erro_404.html')
@@ -30,3 +35,10 @@ def dashboard_content(request):
 
 def cadastro_tanque(request):
     return render(request, 'front/cadastro_tanque.html')
+
+
+
+
+
+
+#CRIAR O SISTEMA de AUTPO DESCOBERTA DE SENSORES ESP32 NA REDE WIFI DA PESSOA
