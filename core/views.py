9013ccc2593d9092a472725e2ac0635d9b4ba8ea
HenriqueEmmanuel <henriqueemmanuel807@gmail.com>
2025-06-27 07:04:46 -0300

    ----

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import bancodedados  # quando criuar o banco coloca aqui
+#from .models import bancodedados  # quando criuar o banco coloca aqui
 
 
 
